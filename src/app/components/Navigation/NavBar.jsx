4b3438773f43496cdb1123754fd464df187dd4ab
lakshanFernando2003 <lakshanchanaka34@gmail.com>
2025-07-01 11:09:35 +0530

    Update NavBar component to include correct navigation items

--- src/app/components/Navigation/NavBar.jsx
+++ src/app/components/Navigation/NavBar.jsx
@@ -20,11 +20,11 @@ export default function NavBar() {
         </div>
         <ul className=" flex flex-row gap-8 items-center pl-2 pr-2">
           <li>home</li>
-          <li>home</li>
-          <li>home</li>
-          <li>home</li>
-          <li>home</li>
-          <li>home</li>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Road Map</li>
+          <li>Services</li>
         </ul>
         <div className='items-center relative '>
           <a className='flex justify-center items-center border border-white/20 bg-[rgb(26_26_26_/_60%)] backdrop-blur-3xl shadow-lg py-2.5 pr-7 pl-7 rounded-full mr-[-0.59rem] hover:bg-[rgb(255_255_255_/_80%)] hover:text-black transition-all duration-[0.4s]' href=""><span className=' text-xs'>Let's Connect</span></a>
